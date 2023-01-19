351c289d533fbbc791efb1ea6ecf273bd029765b
jinseok Ko <eksldpf0508@naver.com>
2023-01-19 23:40:19 +0900

    if문

---
+++ chapter04/if문.py
@@ -0,0 +1,23 @@
+money = True
+if money:
+    print("택시를 탄다")
+else:
+    print("걸어간다")
+print("{:-^60}".format("-"))
+money = 2000
+if money >= 3000:
+    print("택시를 탄다")
+else:
+    print("걸어간다")
+print("{:-^60}".format("-"))
+money = 2000
+card = True
+if money >= 3000 or card:# -> 둘중 하나만 참이어도 참
+    print("택시를 탄다")
+else:
+    print("걸어간다")
+print("{:-^60}".format("-"))
+if money >= 3000 and card:#and -> 모두 참일 경우 참
+    print("택시를 탄다")
+else:
+    print("걸어간다")
