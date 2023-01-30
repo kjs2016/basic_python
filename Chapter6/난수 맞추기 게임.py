4d725d982519766c9a201366908d922e0bb8e54e
jinseok Ko <eksldpf0508@naver.com>
2023-01-30 23:54:16 +0900

    함수를 이용한 높다 낮다 게임 프로그램

---
+++ Chapter6/난수 맞추기 게임.py
@@ -0,0 +1,26 @@
+import random
+
+def game() :
+    print('::::높다 낮다 게임 시작::::')
+    min = 1
+    max = 100
+    count = 5
+    number = random.randint(min, max)
+    print("컴퓨터가 %d부터 %d사이의 난수를 발생 시켰습니다.(Hint:%d)" % (min, max, number))
+
+    while count > 0:
+        print("정답은 %d~%d 사이입니다/." % (min, max))
+        user_num = int(input('시스템이 발생 시킨 난수 값은 무엇일까요 >> '))
+        count -= 1
+
+    if count == 0:
+        print("기회가 소진되어 실격되셨습니다.")
+
+if __name__ == '__main__':
+   while True:
+       game()
+       isPlay = input("다시 시도는 y입력 >> ")
+       if isPlay != 'y':
+           print("수고요~");
+           break
+
